46bed68971acc491dc79db3de6a53176c153815b
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-07-29 08:33:31 -0300

    feat: implement units service into forms component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,8 @@
-import { booleanAttribute, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+import Location from '../../types/location.interface';
+import { UnitsService } from '../../services/units.service';
 
 @Component({
   selector: 'app-form',
@@ -7,15 +10,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrl: './form.component.scss',
 })
 export class FormComponent implements OnInit {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitsService: UnitsService
+  ) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false,
+    });
+
+    this.unitsService.getAllUnits().subscribe((response) => {
+      this.results = response.locations;
+      console.log(this.results);
     });
   }
 
