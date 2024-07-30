79bca06fb97e8cee99e8de9703b08c0e8be0eaf7
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-07-30 10:42:01 -0300

    feat: create legend component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { LegendComponent } from './components/legend/legend.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FormComponent],
+  declarations: [AppComponent, HeaderComponent, FormComponent, LegendComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
